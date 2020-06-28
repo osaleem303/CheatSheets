a8492f62a48ccddccbfd9dd16f6decd1ca4b5b84
Owais Saleem <osaleem303@gmail.com>
2020-06-28 20:51:36 +0500

    Update R.r

--- Languages/R.r
+++ Languages/R.r
@@ -10,7 +10,10 @@
 #**************************************
 
 # 1. Basic Commands
-# 2. File Reading Commands
+# 2. Basic DataTypes
+# 3. File Reading Commands
+# 4. Data Frames
+# 5. ggplot
 
 
 #**************************************
@@ -41,20 +44,65 @@ download.file(URL, dest, method)		#Download file from URL and saves in dest
 										#Params: string, string, string
 										#Note: If the url starts with https on Mac you may need to set method=“curl”
 
+#**************************************
+#   Basic DataTypes 
+#**************************************
+
+#DataTypes                               #Description
+
 
 #**************************************
-#   Basic Commands 
+#   File Reading Commands 
 #**************************************
 
 #Commands                               #Description
 
 #Reading Flat Files
-
+read.table(stringPath)					#Read contents from stringPath into DataFrame
+read.table(stringPath, sep, header)		#Params: string, string (opt), bool (opt)
+										#Note: 'sep' divides the content based on provided character
+										#	   'header' to omit out header contents of file
 
 #Reading Excel
-
+#Required Lib: 'xlsx'
+read.xlsx(stringPath, sheet, header) 	#Reads content from excel file
+										#Params: string, int, bool
+										#Note: 'sheet' is index of excel sheet to be read
+										#	   'header' to omit out header contents of sheet
 
 #Reading XML
-
+#Required Lib: 'XML'
+xmlTreeParse(stringPath, useInternal) 	#Reads XML file from proivided source
+										#Params: string, bool
 
 #Reading JSON
+#Required Lib: 'jsonlite'
+fromJSON(stringPath)					#Reads JSON from stringPath
+
+
+#**************************************
+#   Data Frames
+#**************************************
+
+#Required Lib: 'data.table'
+#Commands                               #Description
+data.frame(x, y, z)						#Creates a data frame provided by data in x, y, z
+
+#Subsetting
+#Rows
+<FrameVariable>[<Condition>, ]			#Splits data of 'FrameVariable' by applying 'Condition'
+#Columns
+<FrameVariable>[ ,<Condition>]
+
+#Adding new Column
+<FrameVariable>[ ,<ColName>:= <data>] 	#Adds Column by 'ColName' with data in 'data'
+
+#Merge
+merge(DataFrame1, DataFrame2)			#Merges DataFrame1 & DataFrame 2 into single DataFrame
+
+#Sort
+sort(dataFrame$var)						#Sort dataframe on basis of variable passed
+sort(dataFrame$var, descending)			#params DataFrame, bool(opt)
+sort(dataFrame$var, na.last)			#Note: descending: If True, sorts in descending order
+										# 	   na.last:		Pushes all NAs at end of list
+
